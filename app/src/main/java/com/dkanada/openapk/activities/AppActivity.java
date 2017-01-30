9fde31c646966e649491c29b5712e3b882956e9b
dkanada <dkanada@users.noreply.github.com>
2017-01-30 00:27:38 -0700

    separate the button updates in AppActivity into their own methods
    I will do the FABs next.

--- app/src/main/java/com/dkanada/openapk/activities/AppActivity.java
+++ app/src/main/java/com/dkanada/openapk/activities/AppActivity.java
@@ -105,11 +105,11 @@ public class AppActivity extends AppCompatActivity {
     TextView version = (TextView) findViewById(R.id.app_version);
     TextView apk = (TextView) findViewById(R.id.app_apk);
     CardView googleplay = (CardView) findViewById(R.id.id_card);
-    CardView start = (CardView) findViewById(R.id.start_card);
+    CardView open = (CardView) findViewById(R.id.start_card);
     CardView extract = (CardView) findViewById(R.id.extract_card);
     CardView uninstall = (CardView) findViewById(R.id.uninstall_card);
     CardView cache = (CardView) findViewById(R.id.cache_card);
-    CardView clearData = (CardView) findViewById(R.id.clear_data_card);
+    CardView data = (CardView) findViewById(R.id.clear_data_card);
     fab = (FloatingActionsMenu) findViewById(R.id.fab);
     FloatingActionButton fab_share = (FloatingActionButton) findViewById(R.id.fab_a);
     final FloatingActionButton fab_hide = (FloatingActionButton) findViewById(R.id.fab_b);
@@ -129,10 +129,15 @@ public class AppActivity extends AppCompatActivity {
     fab_disable.setColorNormal(appPreferences.getFABColorPref());
     fab_disable.setColorPressed(UtilsUI.darker(appPreferences.getFABColorPref(), 0.8));
 
-    // CardView
+    updateOpenButton(open);
+    updateExtractButton(extract);
+    updateUninstallButton(uninstall);
+    updateCacheButton(cache);
+    updateDataButton(data);
+
+    // google play icon
     if (appInfo.isSystem()) {
       icon_googleplay.setVisibility(View.GONE);
-      start.setVisibility(View.GONE);
     } else {
       googleplay.setOnClickListener(new View.OnClickListener() {
         @Override
@@ -145,7 +150,6 @@ public class AppActivity extends AppCompatActivity {
         @Override
         public boolean onLongClick(View view) {
           ClipData clipData;
-
           ClipboardManager clipboardManager = (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
           clipData = ClipData.newPlainText("text", appInfo.getAPK());
           clipboardManager.setPrimaryClip(clipData);
@@ -153,85 +157,6 @@ public class AppActivity extends AppCompatActivity {
           return false;
         }
       });
-
-      start.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-          try {
-            Intent intent = getPackageManager().getLaunchIntentForPackage(appInfo.getAPK());
-            startActivity(intent);
-          } catch (NullPointerException e) {
-            e.printStackTrace();
-            UtilsDialog.showSnackbar(activity, String.format(getResources().getString(R.string.dialog_cannot_open), appInfo.getName()), null, null, 2).show();
-          }
-        }
-      });
-
-      uninstall.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-          Intent intent = new Intent(Intent.ACTION_UNINSTALL_PACKAGE);
-          intent.setData(Uri.parse("package:" + appInfo.getAPK()));
-          intent.putExtra(Intent.EXTRA_RETURN_RESULT, true);
-          startActivityForResult(intent, UNINSTALL_REQUEST_CODE);
-        }
-      });
-    }
-    extract.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        MaterialDialog dialog = UtilsDialog.showTitleContentWithProgress(context
-            , String.format(getResources().getString(R.string.dialog_saving), appInfo.getName())
-            , getResources().getString(R.string.dialog_saving_description));
-        new ExtractFileInBackground(context, dialog, appInfo).execute();
-      }
-    });
-
-    if (UtilsRoot.isRooted()) {
-      if (appInfo.isSystem()) {
-        uninstall.setOnClickListener(new View.OnClickListener() {
-          @Override
-          public void onClick(View view) {
-            MaterialDialog.Builder materialBuilder = UtilsDialog.showUninstall(context)
-                .callback(new MaterialDialog.ButtonCallback() {
-                  @Override
-                  public void onPositive(MaterialDialog dialog) {
-                    MaterialDialog dialogUninstalling = UtilsDialog.showTitleContentWithProgress(context
-                        , String.format(getResources().getString(R.string.dialog_uninstalling), appInfo.getName())
-                        , getResources().getString(R.string.dialog_uninstalling_description));
-                    new UninstallInBackground(context, dialogUninstalling, appInfo).execute();
-                    dialog.dismiss();
-                  }
-                });
-            materialBuilder.show();
-          }
-        });
-      }
-      cache.setVisibility(View.VISIBLE);
-      cache.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-          MaterialDialog dialog = UtilsDialog.showTitleContentWithProgress(context
-              , getResources().getString(R.string.dialog_cache_deleting)
-              , getResources().getString(R.string.dialog_cache_deleting_description));
-          new DeleteDataInBackground(context, dialog, appInfo.getData() + "/cache/**"
-              , getResources().getString(R.string.dialog_cache_success_description, appInfo.getName())).execute();
-        }
-      });
-      clearData.setVisibility(View.VISIBLE);
-      clearData.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-          MaterialDialog dialog = UtilsDialog.showTitleContentWithProgress(context
-              , getResources().getString(R.string.dialog_clear_data_deleting)
-              , getResources().getString(R.string.dialog_clear_data_deleting_description));
-          new DeleteDataInBackground(context, dialog, appInfo.getData() + "/**"
-              , getResources().getString(R.string.dialog_clear_data_success_description, appInfo.getName())).execute();
-        }
-      });
-    } else if (appInfo.isSystem()) {
-      uninstall.setVisibility(View.GONE);
-      uninstall.setForeground(null);
     }
 
     // FAB (Share)
@@ -295,7 +220,104 @@ public class AppActivity extends AppCompatActivity {
         }
       });
     }
+  }
 
+  protected void updateOpenButton(CardView open) {
+    if (appInfo.isSystem()) {
+      open.setVisibility(View.GONE);
+    } else {
+      open.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+          try {
+            Intent intent = getPackageManager().getLaunchIntentForPackage(appInfo.getAPK());
+            startActivity(intent);
+          } catch (NullPointerException e) {
+            e.printStackTrace();
+            UtilsDialog.showSnackbar(activity, String.format(getResources().getString(R.string.dialog_cannot_open), appInfo.getName()), null, null, 2).show();
+          }
+        }
+      });
+    }
+  }
+
+  protected void updateExtractButton(CardView extract) {
+    extract.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        MaterialDialog dialog = UtilsDialog.showTitleContentWithProgress(context
+            , String.format(getResources().getString(R.string.dialog_saving), appInfo.getName())
+            , getResources().getString(R.string.dialog_saving_description));
+        new ExtractFileInBackground(context, dialog, appInfo).execute();
+      }
+    });
+  }
+
+  protected void updateUninstallButton(CardView uninstall) {
+    if (appInfo.isSystem() && UtilsRoot.isRooted()) {
+      uninstall.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+          MaterialDialog.Builder materialBuilder = UtilsDialog.showUninstall(context)
+              .callback(new MaterialDialog.ButtonCallback() {
+                @Override
+                public void onPositive(MaterialDialog dialog) {
+                  MaterialDialog dialogUninstalling = UtilsDialog.showTitleContentWithProgress(context
+                      , String.format(getResources().getString(R.string.dialog_uninstalling), appInfo.getName())
+                      , getResources().getString(R.string.dialog_uninstalling_description));
+                  new UninstallInBackground(context, dialogUninstalling, appInfo).execute();
+                  dialog.dismiss();
+                }
+              });
+          materialBuilder.show();
+        }
+      });
+    } else if(appInfo.isSystem()) {
+      uninstall.setVisibility(View.GONE);
+      uninstall.setForeground(null);
+    } else {
+      uninstall.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+          Intent intent = new Intent(Intent.ACTION_UNINSTALL_PACKAGE);
+          intent.setData(Uri.parse("package:" + appInfo.getAPK()));
+          intent.putExtra(Intent.EXTRA_RETURN_RESULT, true);
+          startActivityForResult(intent, UNINSTALL_REQUEST_CODE);
+        }
+      });
+    }
+  }
+
+  protected void updateCacheButton(CardView cache) {
+    if (UtilsRoot.isRooted()) {
+      cache.setVisibility(View.VISIBLE);
+      cache.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+          MaterialDialog dialog = UtilsDialog.showTitleContentWithProgress(context
+              , getResources().getString(R.string.dialog_cache_deleting)
+              , getResources().getString(R.string.dialog_cache_deleting_description));
+          new DeleteDataInBackground(context, dialog, appInfo.getData() + "/cache/**"
+              , getResources().getString(R.string.dialog_cache_success_description, appInfo.getName())).execute();
+        }
+      });
+    }
+  }
+
+  protected void updateDataButton(CardView data) {
+    if (UtilsRoot.isRooted()) {
+      data.setVisibility(View.VISIBLE);
+      data.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+          MaterialDialog dialog = UtilsDialog.showTitleContentWithProgress(context
+              , getResources().getString(R.string.dialog_clear_data_deleting)
+              , getResources().getString(R.string.dialog_clear_data_deleting_description));
+          new DeleteDataInBackground(context, dialog, appInfo.getData() + "/**"
+              , getResources().getString(R.string.dialog_clear_data_success_description, appInfo.getName())).execute();
+        }
+      });
+    }
   }
 
   @Override
