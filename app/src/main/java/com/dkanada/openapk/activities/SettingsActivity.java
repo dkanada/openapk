3723cec9579103a991567a563fff8dfa97656883
dkanada <dkanada@users.noreply.github.com>
2017-02-05 18:07:40 -0700

    move around methods in settings for clarity

--- app/src/main/java/com/dkanada/openapk/activities/SettingsActivity.java
+++ app/src/main/java/com/dkanada/openapk/activities/SettingsActivity.java
@@ -1,5 +1,6 @@
 package com.dkanada.openapk.activities;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -50,17 +51,19 @@ public class SettingsActivity extends PreferenceActivity implements SharedPrefer
     SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
     prefs.registerOnSharedPreferenceChangeListener(this);
 
-    prefVersion = findPreference("prefVersion");
-    prefLicense = findPreference("prefLicense");
-    prefPrimaryColor = (AmbilWarnaPreference) findPreference("prefPrimaryColor");
-    prefFABColor = (AmbilWarnaPreference) findPreference("prefFABColor");
     prefDeleteAll = findPreference("prefDeleteAll");
-    prefDefaultValues = findPreference("prefDefaultValues");
-    prefNavigationBlack = findPreference("prefNavigationBlack");
+    prefCustomPath = findPreference("prefCustomPath");
     prefCustomFilename = (ListPreference) findPreference("prefCustomFilename");
     prefSortMode = (ListPreference) findPreference("prefSortMode");
+
     prefTheme = (ListPreference) findPreference("prefTheme");
-    prefCustomPath = findPreference("prefCustomPath");
+    prefPrimaryColor = (AmbilWarnaPreference) findPreference("prefPrimaryColor");
+    prefFABColor = (AmbilWarnaPreference) findPreference("prefFABColor");
+    prefNavigationBlack = findPreference("prefNavigationBlack");
+    prefDefaultValues = findPreference("prefDefaultValues");
+
+    prefVersion = findPreference("prefVersion");
+    prefLicense = findPreference("prefLicense");
 
     setInitialConfiguration();
 
@@ -81,16 +84,10 @@ public class SettingsActivity extends PreferenceActivity implements SharedPrefer
       }
     });
 
-    // prefCustomFilename
+    // set a few preference summaries
     setCustomFilenameSummary();
-
-    // prefSortMode
     setSortModeSummary();
-
-    // prefCustomPath
     setCustomPathSummary();
-
-    // prefTheme
     setThemeSummary();
 
     // prefDeleteAll
@@ -126,7 +123,7 @@ public class SettingsActivity extends PreferenceActivity implements SharedPrefer
     ViewGroup contentView = (ViewGroup) LayoutInflater.from(this).inflate(R.layout.activity_settings, new LinearLayout(this), false);
     toolbar = (Toolbar) contentView.findViewById(R.id.toolbar);
     //TODO Toolbar should load the default style in XML (white title and back arrow), but doesn't happen
-    toolbar.setTitleTextColor(getResources().getColor(R.color.textWhite));
+    //toolbar.setTitleTextColor(getResources().getColor(R.color.textWhite));
     toolbar.setNavigationOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View view) {
@@ -160,16 +157,6 @@ public class SettingsActivity extends PreferenceActivity implements SharedPrefer
     }
   }
 
-  private void setCustomFilenameSummary() {
-    int filenameValue = Integer.valueOf(appPreferences.getCustomFilename()) - 1;
-    prefCustomFilename.setSummary(getResources().getStringArray(R.array.filenameEntries)[filenameValue]);
-  }
-
-  private void setSortModeSummary() {
-    int sortValue = Integer.valueOf(appPreferences.getSortMode()) - 1;
-    prefSortMode.setSummary(getResources().getStringArray(R.array.sortEntries)[sortValue]);
-  }
-
   private void setCustomPathSummary() {
     String path = appPreferences.getCustomPath();
     if (path.equals(UtilsApp.getDefaultAppFolder().getPath())) {
@@ -180,6 +167,16 @@ public class SettingsActivity extends PreferenceActivity implements SharedPrefer
     }
   }
 
+  private void setCustomFilenameSummary() {
+    int filenameValue = Integer.valueOf(appPreferences.getCustomFilename()) - 1;
+    prefCustomFilename.setSummary(getResources().getStringArray(R.array.filenameEntries)[filenameValue]);
+  }
+
+  private void setSortModeSummary() {
+    int sortValue = Integer.valueOf(appPreferences.getSortMode()) - 1;
+    prefSortMode.setSummary(getResources().getStringArray(R.array.sortEntries)[sortValue]);
+  }
+
   private void setThemeSummary(){
     int sortValue = Integer.valueOf(appPreferences.getTheme()) - 1;
     prefTheme.setSummary(getResources().getStringArray(R.array.themeEntries)[sortValue]);
@@ -188,12 +185,12 @@ public class SettingsActivity extends PreferenceActivity implements SharedPrefer
   @Override
   public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
     Preference pref = findPreference(key);
-    if (pref == prefCustomFilename) {
+    if (pref == prefCustomPath) {
+      setCustomPathSummary();
+    } else if (pref == prefCustomFilename) {
       setCustomFilenameSummary();
     } else if (pref == prefSortMode) {
       setSortModeSummary();
-    } else if (pref == prefCustomPath) {
-      setCustomPathSummary();
     } else if (pref == prefTheme) {
       setThemeSummary();
     }
