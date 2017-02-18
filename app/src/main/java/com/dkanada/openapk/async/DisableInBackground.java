a6dac014d6de791c5704810ff0488979bd3c8a11
dkanada <dkanada@users.noreply.github.com>
2017-02-17 21:17:09 -0700

    add initial code for hide and disable async

--- app/src/main/java/com/dkanada/openapk/async/DisableInBackground.java
+++ app/src/main/java/com/dkanada/openapk/async/DisableInBackground.java
@@ -9,14 +9,15 @@ import com.dkanada.openapk.AppInfo;
 import com.dkanada.openapk.R;
 import com.dkanada.openapk.utils.UtilsApp;
 import com.dkanada.openapk.utils.UtilsDialog;
+import com.dkanada.openapk.utils.UtilsRoot;
 
-public class ExtractFileInBackground extends AsyncTask<Void, String, Boolean> {
+public class DisableInBackground extends AsyncTask<Void, String, Boolean> {
   private Context context;
   private Activity activity;
   private MaterialDialog dialog;
   private AppInfo appInfo;
 
-  public ExtractFileInBackground(Context context, MaterialDialog dialog, AppInfo appInfo) {
+  public DisableInBackground(Context context, MaterialDialog dialog, AppInfo appInfo) {
     this.context = context;
     this.activity = (Activity) context;
     this.dialog = dialog;
@@ -27,7 +28,7 @@ public class ExtractFileInBackground extends AsyncTask<Void, String, Boolean> {
   protected Boolean doInBackground(Void... voids) {
     Boolean status = false;
     if (UtilsApp.checkPermissions(activity)) {
-      status = UtilsApp.copyFile(appInfo);
+      status = UtilsRoot.clearDataWithRootPermission("temp");
     }
     return status;
   }
@@ -37,9 +38,9 @@ public class ExtractFileInBackground extends AsyncTask<Void, String, Boolean> {
     super.onPostExecute(status);
     dialog.dismiss();
     if (status) {
-      UtilsDialog.showSnackBar(activity, String.format(context.getResources().getString(R.string.dialog_saved_description), appInfo.getName(), UtilsApp.getAPKFilename(appInfo)), context.getResources().getString(R.string.button_undo), UtilsApp.getOutputFilename(appInfo), 1).show();
+      UtilsDialog.showSnackBar(activity, "temp", null, null, 2).show();
     } else {
-      UtilsDialog.showTitleContent(context, context.getResources().getString(R.string.dialog_extract_fail), context.getResources().getString(R.string.dialog_extract_fail_description));
+      UtilsDialog.showTitleContent(context, context.getResources().getString(R.string.dialog_root_required), context.getResources().getString(R.string.dialog_root_required_description));
     }
   }
 }
